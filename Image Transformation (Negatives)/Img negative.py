4cb09aa30cc77fcebfccf9ea0179c27f493f7025
Haider Ali <42317018+AliHaider20@users.noreply.github.com>
2021-02-23 12:01:04 +0530

    Add files via upload

---
+++ Image Transformation (Negatives)/Img negative.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import numpy as np
+img = plt.imread('Original.jpg')
+
+img = img.copy()
+
+maxPixs = []
+for row in img[:,:,0]:
+   maxPixs.append(max(row))
+
+maxPix = max(maxPixs)
+
+for gray_intensity in range(10):
+   if (2**gray_intensity-1) == maxPix:
+      break
+L = 2**gray_intensity - 1
+print("Image Intensity :",L)
+choice = input('According to which channel Img negative is needed red, blue, green or all: ')
+choice = choice.lower()
+def MakeNeg (img,L,channel):
+   if channel == 'red':
+      ch = img[:,:,0]
+      for row in range(len(ch[:])):
+         for col in range(len(ch[0,:])):
+            ch[row,col] = 255 - ch[row,col]
+
+   if channel == 'green':
+      ch = img[:,:,1]
+      for row in range(len(ch[:])):
+         for col in range(len(ch[0,:])):
+            ch[row,col] = 255 - ch[row,col]
+
+   if channel == 'blue':
+      ch = img[:,:,2]
+      for row in range(len(ch[:])):
+         for col in range(len(ch[0,:])):
+            ch[row,col] = 255 - ch[row,col]
+
+   else:
+      ch = img[:,:,0]
+      for row in range(len(ch[:])):
+         for col in range(len(ch[0,:])):
+            ch[row,col] = 255 - ch[row,col]
+
+      ch = img[:,:,1]
+      for row in range(len(ch[:])):
+         for col in range(len(ch[0,:])):
+            ch[row,col] = 255 - ch[row,col]
+
+      ch = img[:,:,2]
+      for row in range(len(ch[:])):
+         for col in range(len(ch[0,:])):
+            ch[row,col] = 255 - ch[row,col]
+
+   return ch
+
+channel_neg = MakeNeg(img,L,choice)
+plt.imsave('Img Negative acc to {0} channel.jpg'.format(choice),img)
